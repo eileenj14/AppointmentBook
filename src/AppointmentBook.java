aedb1c4ab437fe746ab8493857b1df0692bb6b2e
Eileen Jiang <eileenj14@nycstudents.net>
2025-09-26 11:51:23 -0400

    AppointmentBook start.

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -1,5 +1,18 @@
 public class AppointmentBook
 {
+    private boolean[][] schedule;
+
+    public AppointmentBook(boolean[][] schedule)
+    {
+        this.schedule = schedule;
+    }
+
+    public void printPeriod(int period)
+    {
+        for(int i = 0; i < schedule[period - 1].length; i++)
+            System.out.println(i + " " + schedule[period - 1][i]);
+    }
+
     /**
      * Returns true if minute in period is available for an appointment and returns
      * false otherwise
@@ -7,7 +20,7 @@ public class AppointmentBook
      */
     private boolean isMinuteFree(int period, int minute)
     {
-        return true;
+        return schedule[period - 1][minute];
     }
 
     /**
@@ -29,7 +42,15 @@ public class AppointmentBook
      */
     public int findFreeBlock(int period, int duration)
     {
-        return 0;
+        int block = 0;
+        for(int i = 0; i < 60; i++)
+            if(isMinuteFree(period, i))
+            {
+                block++;
+                if(block == duration) return i - duration + 1;
+            }
+            else block = 0;
+        return -1;
     }
 
     /**
@@ -41,6 +62,6 @@ public class AppointmentBook
      */
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration)
     {
-        return true;
+        return false;
     }
 }
